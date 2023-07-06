aeab2e705dccc342ed037740363b72776baa15d6
User <User@DESKTOP-ABL862Q>
2023-07-06 21:22:39 +0700

    Add project files.

---
+++ FunctionCheckIPv4.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ip_Validator_Implementation
+{
+    internal class FunctionCheckIPv4
+    {
+        public static (bool, string) IsValidIPv4Address(string ipAddress)
+        {
+            // Split the IP address into octets
+            string[] octets = ipAddress.Split('.');
+
+            // Check if the IP address has exactly 4 octets
+            if (octets.Length != 4)
+            {
+                return (false, "Invalid IP address. IP address must consist of four octets.");
+            }
+
+            // Check each octet for validity
+            foreach (string octet in octets)
+            {
+                // Check if the octet is empty or starts with '0'
+                if (string.IsNullOrEmpty(octet) || (octet.Length > 1 && octet[0] == '0'))
+                {
+                    return (false, $"Invalid IP address. Octet '{octet}' is not valid.");
+                }
+
+                // Parse the octet as an integer
+                if (!int.TryParse(octet, out int value))
+                {
+                    return (false, $"Invalid IP address. Octet '{octet}' is not a valid integer.");
+                }
+
+                // Check if the octet is within the valid range (0-255)
+                if (value < 0 || value > 255)
+                {
+                    return (false, $"Invalid IP address. Octet '{octet}' is not within the valid range (0-255).");
+                }
+            }
+
+            // If all checks pass, the IP address is valid
+            return (true, "Valid IP address.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Ip_Validator_Implementation.FunctionCheckIPv4;
+
+namespace Ip_Validator_Implementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ipAddress = "192.168.0.256";
+            (bool isValid, string message) = IsValidIPv4Address(ipAddress);
+            Console.WriteLine($"Is {ipAddress} a valid IPv4 address?");
+            if (isValid == true)
+            {
+                Console.WriteLine($"Isvalid : {isValid} \nMessage : {message}");
+            }
+            else
+            {
+                Console.WriteLine($"Isvalid : {isValid} \nMessage : {message}");
+            }
+        }
+    }
+}
